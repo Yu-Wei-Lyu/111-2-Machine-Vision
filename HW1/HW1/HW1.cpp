797b56b3a8de5c6453debe025fe0df3ff9d66cc9
Yu-Wei,Lyu <awaia732@gmail.com>
2023-03-13 15:16:55 +0800

    HW1 Q1-3 (undone)

--- HW1/HW1/HW1.cpp
+++ HW1/HW1/HW1.cpp
@@ -1,84 +1,98 @@
 #include<opencv2/opencv.hpp>
 #include<direct.h>
-#define INDEX_COLOR_PATH  "./Image/Index color/"
-#define RESIZE_PATH  "./Image/Resize/"
-#define RESIZE_INTERPOLATION_PATH  "./Image/Resize(interpolation)/"
 
 using namespace std;
 using namespace cv;
 
-void grayScaleImage(string fileName, bool showImage = true);
-void binaryImage(string fileName, bool showImage = true);
-void indexColorImage(string fileName, bool showImage = true);
+Mat getGrayScaleImage(const Mat& image);
+Mat getBinaryImage(const Mat& image);
+Mat getIndexColorImage(const Mat& image, Mat& lookUpTable);
 
 int main()
 {
+	const string SOURCE_FOLDER = "./Image/Source/";
+	const string GRAYSCALE_FOLDER = "./Image/Grayscale/";
+	const string INDEX_COLOR_FOLDER = "./Image/Index color/";
+	const string RESIZE_FOLDER = "./Image/Resize/";
+	const string RESIZE_INTERPOLATION_FOLDER = "./Image/Resize(interpolation)/";
 	vector<string> imageList = { "House256.png", "House512.png", "JellyBeans.png", "Lena.png", "Mandrill.png", "Peppers.png" };
-	//binaryImage(image_name.at(0), true);
-	for (string& imageName : imageList) {
-		indexColorImage(imageName, true);
-	}
+	Mat image = imread(SOURCE_FOLDER + imageList.at(0));
+	imshow("Source", image);
+	Mat gray = getGrayScaleImage(image);
+	imshow("Gray", gray);
+	Mat binary = getBinaryImage(image);
+	imshow("Binary", binary);
+	Mat lookUpTable = Mat(16, 16, CV_8UC3);
+	getIndexColorImage(image, lookUpTable);
+	//Mat indexColor = getIndexColorImage(image, lookUpTable);
+	//imshow("Binary", binary);
+	
+	//grayScaleImage(image, imageList.at(0), true);
+	//for (string& imageName : imageList) {
+	//	Mat image = imread(SOURCE_FOLDER + imageName);
+	//	Mat gray = grayScaleImage(image);
+	//	imwrite("./Image/Grayscale/" + imageName, gray);
+	//}
 	waitKey();
 	destroyAllWindows();
 	return 0;
 }
 
-void grayScaleImage(string fileName, bool showImage) {
-	const string SOURCE_FOLDER = "./Image/Source/";
-	const string GRAYSCALE_FOLDER = "./Image/Grayscale/";
-	Mat image = imread(SOURCE_FOLDER + fileName);
-	Mat grayscaleImage = Mat(image.rows, image.cols, CV_8UC1);
-	uchar* imagePtr = image.ptr<uchar>(0);
-	uchar* gray = grayscaleImage.ptr<uchar>(0);
+Mat getGrayScaleImage(const Mat& image) {
+	Mat grayImage = Mat(image.rows, image.cols, CV_8UC1);
+	const uchar* imagePtr = image.ptr<uchar>(0);
+	uchar* gray = grayImage.ptr<uchar>(0);
 	int pixels = image.rows * image.cols;
 	for (int pixel = 0; pixel < pixels; pixel++) {
 		uchar blue = *imagePtr++, green = *imagePtr++, red = *imagePtr++;
 		*gray++ = (0.3 * red) + (0.59 * green) + (0.11 * blue);
 	}
-	if (showImage) {
-		imshow(SOURCE_FOLDER + fileName, image);
-		imshow(GRAYSCALE_FOLDER + fileName, grayscaleImage);
-	}
-	imwrite(GRAYSCALE_FOLDER + fileName, grayscaleImage);
+	return grayImage;
 }
 
-void binaryImage(string fileName, bool showImage) {
-	const string GRAYSCALE_FOLDER = "./Image/Grayscale/";
-	const string BINARY_FOLDER = "./Image/Binary/";
-	Mat image = imread(GRAYSCALE_FOLDER + fileName);
+Mat getBinaryImage(const Mat& image) {
+	Mat grayImage = image;
+	if (image.channels() == 3) {
+		grayImage = getGrayScaleImage(image);
+	}
 	Mat binaryImage = Mat(image.rows, image.cols, CV_8UC1);
-	uchar* imagePtr = image.ptr<uchar>(0);
+	const uchar* imagePtr = grayImage.ptr<uchar>(0);
 	uchar* binary = binaryImage.ptr<uchar>(0);
 	int pixels = image.rows * image.cols;
 	for (int pixel = 0; pixel < pixels; pixel++) {
 		*binary++ = (*imagePtr >= 128) ? 255 : 0;
-		imagePtr = imagePtr + image.channels();
+		imagePtr++;
 	}
-	if (showImage) {
-		imshow(GRAYSCALE_FOLDER + fileName, image);
-		imshow(BINARY_FOLDER + fileName, binaryImage);
-	}
-	imwrite(BINARY_FOLDER + fileName, binaryImage);
+	return binaryImage;
 }
 
-void indexColorImage(string fileName, bool showImage) {
-	const string SOURCE_FOLDER = "./Image/Source/";
-	const string INDEX_COLOR_FOLDER = "./Image/Index color/";
-	Mat image = imread(SOURCE_FOLDER + fileName);
-	Mat myLUT = Mat(16, 16, CV_8UC1);
+Mat getIndexColorImage(const Mat& image, Mat& lookUpTable) {
 	Mat resultImage = Mat(image.rows, image.cols, CV_8UC1);
-	uchar* myLUTPtr = myLUT.ptr<uchar>(0);
-	uchar* imagePtr = image.ptr<uchar>(0);
+	const uchar* imagePtr = image.ptr<uchar>(0);
 	uchar* resultPtr = resultImage.ptr<uchar>(0);
 	int pixels = image.rows * image.cols;
+	lookUpTable.at<Vec3b>(0, 0) = image.at<Vec3b>(0, 0);
+	cout << (int)lookUpTable.at<Vec3b>(0, 0)[0] << "," << (int)lookUpTable.at<Vec3b>(0,0)[1] << "," << (int)lookUpTable.at<Vec3b>(0, 0)[2] << endl;
+	cout << (int)image.at<Vec3b>(0, 0)[0] << "," << (int)image.at<Vec3b>(0, 0)[1] << "," << (int)image.at<Vec3b>(0, 0)[2] << endl;
+	return resultImage;
+	int LUTPixels = 1;
 	for (int pixel = 0; pixel < pixels; pixel++) {
 		uchar blue = *imagePtr++, green = *imagePtr++, red = *imagePtr++;
-		*resultPtr++ = (*imagePtr >= 128) ? 255 : 0;
-		imagePtr = imagePtr + image.channels();
+		uchar* lookUpPtr = lookUpTable.ptr<uchar>(0);
+		bool IsAtLUT = false;
+		for (int LUTPixel = 0; LUTPixel < LUTPixels; LUTPixel++) {
+			uchar LUTBlue = *lookUpPtr++, LUTGreen = *lookUpPtr++, LUTRed = *lookUpPtr++;
+			if (blue == LUTBlue && green == LUTGreen && red == LUTRed) {
+				*resultPtr = LUTPixel;
+			}
+
+		}
+		if (!IsAtLUT) {
+			Vec3b* indexColorPixel = &lookUpTable.at<Vec3b>(pixel / 16, pixel % 16);
+			indexColorPixel[0] = blue;
+			indexColorPixel[1] = green;
+			indexColorPixel[2] = red;
+		}
 	}
-	if (showImage) {
-		imshow(SOURCE_FOLDER + fileName, image);
-		imshow(INDEX_COLOR_FOLDER + fileName, resultImage);
-	}
-	imwrite(INDEX_COLOR_FOLDER + fileName, resultImage);
+	return resultImage;
 }
