824a9e9833a3cb8eafd14ce19be7b36337cdd77a
Yu-Wei,Lyu <awaia732@gmail.com>
2023-03-14 11:06:29 +0800

    Pre-code

--- HW1/HW1/HW1.cpp
+++ HW1/HW1/HW1.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 using namespace cv;
 
+void prepareFolder();
 Mat getMyColorImage();
 Mat getGrayScaleImage(const Mat& image);
 Mat getBinaryImage(const Mat& image);
@@ -16,7 +17,8 @@ int main()
 	const string BINARY_FOLDER = "./Image/Binary/";
 	const string INDEX_COLOR_FOLDER = "./Image/Index color/";
 	const string RESIZE_FOLDER = "./Image/Resize/";
-	const string RESIZE_INTERPOLATION_FOLDER = "./Image/Resize(interpolation)/";
+	string RESIZE_INTERPOLATION_FOLDER = "./Image/Resize(interpolation)/";
+	prepareFolder();
 	vector<string> imageList = { "House256.png", "House512.png", "JellyBeans.png", "Lena.png", "Mandrill.png", "Peppers.png" };
 	//vector<string> imageList = { "Lena.png" };
 	Mat colorMap = getMyColorImage();
@@ -26,7 +28,7 @@ int main()
 		Mat gray = getGrayScaleImage(image);
 		imshow("Gray", gray);
 		imwrite(GRAYSCALE_FOLDER + imageName, gray);
-		Mat binary = getBinaryImage(image);
+		Mat binary = getBinaryImage(gray);
 		imshow("Binary", binary);
 		imwrite(BINARY_FOLDER + imageName, binary);
 		Mat indexColor = getIndexColorImage(image);
@@ -36,6 +38,16 @@ int main()
 	waitKey();
 	destroyAllWindows();
 	return 0;
+}
+
+void prepareFolder() {
+	_mkdir("./Image/");
+	_mkdir("./Image/Source/");
+	_mkdir("./Image/Grayscale/");
+	_mkdir("./Image/Binary/");
+	_mkdir("./Image/Index color/");
+	_mkdir("./Image/Resize/");
+	_mkdir("./Image/Resize(interpolation)/");
 }
 
 Mat getMyColorImage() {
@@ -54,12 +66,16 @@ Mat getMyColorImage() {
 
 Mat getGrayScaleImage(const Mat& image) {
 	Mat grayImage = Mat(image.rows, image.cols, CV_8UC1);
-	const uchar* imagePtr = image.ptr<uchar>(0);
-	uchar* gray = grayImage.ptr<uchar>(0);
-	int pixels = image.rows * image.cols;
-	for (int pixel = 0; pixel < pixels; pixel++) {
-		uchar blue = *imagePtr++, green = *imagePtr++, red = *imagePtr++;
-		*gray++ = (0.3 * red) + (0.59 * green) + (0.11 * blue);
+	int rows = image.rows * image.cols;
+	const uchar* imagePtr;
+	uchar* gray;
+	for (int row = 0; row < image.rows; row++) {
+		imagePtr = image.ptr<uchar>(row);
+		gray = grayImage.ptr<uchar>(row);
+		for (int col = 0; col < image.cols; col++) {
+			uchar blue = *imagePtr++, green = *imagePtr++, red = *imagePtr++;
+			*gray++ = (0.3 * red) + (0.59 * green) + (0.11 * blue);
+		}
 	}
 	return grayImage;
 }
@@ -72,8 +88,8 @@ Mat getBinaryImage(const Mat& image) {
 	Mat binaryImage = Mat(image.rows, image.cols, CV_8UC1);
 	const uchar* imagePtr = grayImage.ptr<uchar>(0);
 	uchar* binary = binaryImage.ptr<uchar>(0);
-	int pixels = image.rows * image.cols;
-	for (int pixel = 0; pixel < pixels; pixel++) {
+	int rows = image.rows * image.cols;
+	for (int row = 0; row < image.rows; row++) {
 		*binary++ = (*imagePtr >= 128) ? 255 : 0;
 		imagePtr++;
 	}
@@ -84,8 +100,8 @@ Mat getIndexColorImage(const Mat& image) {
 	Mat resultImage = Mat(image.rows, image.cols, CV_8UC3);
 	const uchar* imagePtr = image.ptr<uchar>(0);
 	uchar* resultPtr = resultImage.ptr<uchar>(0);
-	int pixels = image.rows * image.cols * image.channels();
-	for (int pixel = 0; pixel < pixels; pixel++) {
+	int rows = image.rows * image.cols * image.channels();
+	for (int row = 0; row < image.rows; row++) {
 		int remain = *imagePtr % 51;
 		*resultPtr = *imagePtr - remain;
 		if (remain >= 25)
