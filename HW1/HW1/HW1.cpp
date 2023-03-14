1964a59edfcc1638dd3eb454b2d0fd00c4aa53bf
Yu-Wei, Lyu <awaia732@gmail.com>
2023-03-14 15:16:58 +0800

    HW3 Q1-3 Re-code (undone)

--- HW1/HW1/HW1.cpp
+++ HW1/HW1/HW1.cpp
@@ -4,69 +4,51 @@
 using namespace std;
 using namespace cv;
 
-void prepareFolder();
-Mat getMyColorImage();
+void prepareFolder(vector<string> folderList);
 Mat getGrayScaleImage(const Mat& image);
 Mat getBinaryImage(const Mat& image);
-Mat getIndexColorImage(const Mat& image);
+
+Vec3b getColorFromMap(Mat& colorMap, const Vec3b bgr, int& threshould, int& colorPixels);
+Mat getIndexColorImage(const Mat& image, Mat& colorMap);
 
 int main()
 {
-	const string SOURCE_FOLDER = "./Image/Source/";
-	const string GRAYSCALE_FOLDER = "./Image/Grayscale/";
-	const string BINARY_FOLDER = "./Image/Binary/";
-	const string INDEX_COLOR_FOLDER = "./Image/Index color/";
-	const string RESIZE_FOLDER = "./Image/Resize/";
-	string RESIZE_INTERPOLATION_FOLDER = "./Image/Resize(interpolation)/";
-	prepareFolder();
-	vector<string> imageList = { "House256.png", "House512.png", "JellyBeans.png", "Lena.png", "Mandrill.png", "Peppers.png" };
-	//vector<string> imageList = { "Lena.png" };
-	Mat colorMap = getMyColorImage();
+	vector<string> folderList = { "./Image/Source/", "./Image/Grayscale/", "./Image/Binary/", "./Image/Index color/", "./Image/Resize/", "./Image/Resize(interpolation)/" };
+	//vector<string> imageList = { "House256.png", "House512.png", "JellyBeans.png", "Lena.png", "Mandrill.png", "Peppers.png" };
+	vector<string> imageList = { "House512.png" };
+	prepareFolder(folderList);
+
 	for (string& imageName : imageList) {
-		Mat image = imread(SOURCE_FOLDER + imageName);
+		Mat image = imread(folderList.at(0) + imageName);
 		imshow("Source", image);
-		Mat gray = getGrayScaleImage(image);
+		/*Mat gray = getGrayScaleImage(image);
 		imshow("Gray", gray);
-		imwrite(GRAYSCALE_FOLDER + imageName, gray);
+		imwrite(folderList.at(1) + imageName, gray);
 		Mat binary = getBinaryImage(gray);
 		imshow("Binary", binary);
-		imwrite(BINARY_FOLDER + imageName, binary);
-		Mat indexColor = getIndexColorImage(image);
-		imshow("Index-Color", indexColor);
-		imwrite(INDEX_COLOR_FOLDER + imageName, indexColor);
+		imwrite(folderList.at(2) + imageName, binary);*/
+		Mat myColorMap;
+		Mat indexColor2 = getIndexColorImage(image, myColorMap);
+		imshow("Index-Color-Limit", indexColor2);
+		imshow("Color-Map-Limit", myColorMap);
+		imwrite(folderList.at(3) + "2_" + imageName, indexColor2);
+		imwrite(folderList.at(3) + "2_color_map_" + imageName, myColorMap);
 	}
 	waitKey();
 	destroyAllWindows();
 	return 0;
 }
 
-void prepareFolder() {
-	_mkdir("./Image/");
-	_mkdir("./Image/Source/");
-	_mkdir("./Image/Grayscale/");
-	_mkdir("./Image/Binary/");
-	_mkdir("./Image/Index color/");
-	_mkdir("./Image/Resize/");
-	_mkdir("./Image/Resize(interpolation)/");
-}
-
-Mat getMyColorImage() {
-	Mat myColorMap = Mat(16, 16, CV_8UC3);
-	uchar* p = myColorMap.ptr<uchar>(0);
-	for (int i = 0; i < 256; i += 51) {
-		for (int j = 0; j < 256; j += 51) {
-			for (int k = 0; k < 256; k += 51) {
-				*p++ = i, *p++ = j, *p++ = k;
-			}
+void prepareFolder(vector<string> folderList) {
+	for (string& folder : folderList) {
+		if (_mkdir(folder.c_str()) != 0) {
+			cerr << strerror(errno) << ", file path " << folder.c_str() << endl;
 		}
 	}
-	imwrite("./Image/myColorMap.png", myColorMap);
-	return myColorMap;
 }
 
 Mat getGrayScaleImage(const Mat& image) {
 	Mat grayImage = Mat(image.rows, image.cols, CV_8UC1);
-	int rows = image.rows * image.cols;
 	const uchar* imagePtr;
 	uchar* gray;
 	for (int row = 0; row < image.rows; row++) {
@@ -82,32 +64,93 @@ Mat getGrayScaleImage(const Mat& image) {
 
 Mat getBinaryImage(const Mat& image) {
 	Mat grayImage = image;
-	if (image.channels() == 3) {
+	if (image.channels() == 3)
 		grayImage = getGrayScaleImage(image);
-	}
 	Mat binaryImage = Mat(image.rows, image.cols, CV_8UC1);
-	const uchar* imagePtr = grayImage.ptr<uchar>(0);
-	uchar* binary = binaryImage.ptr<uchar>(0);
-	int rows = image.rows * image.cols;
-	for (int row = 0; row < image.rows; row++) {
-		*binary++ = (*imagePtr >= 128) ? 255 : 0;
-		imagePtr++;
+	const uchar* imagePtr;
+	uchar* binary;
+	for (int row = 0; row < grayImage.rows; row++) {
+		imagePtr = grayImage.ptr<uchar>(row);
+		binary = binaryImage.ptr<uchar>(row);
+		for (int col = 0; col < grayImage.cols; col++) {
+			*binary++ = (*imagePtr++ >= 128) ? 255 : 0;
+		}
 	}
 	return binaryImage;
 }
 
-Mat getIndexColorImage(const Mat& image) {
-	Mat resultImage = Mat(image.rows, image.cols, CV_8UC3);
-	const uchar* imagePtr = image.ptr<uchar>(0);
-	uchar* resultPtr = resultImage.ptr<uchar>(0);
-	int rows = image.rows * image.cols * image.channels();
-	for (int row = 0; row < image.rows; row++) {
-		int remain = *imagePtr % 51;
-		*resultPtr = *imagePtr - remain;
-		if (remain >= 25)
-			*resultPtr += 51;
-		resultPtr++;
-		imagePtr++;
+Vec3b getColorFromMap(Mat& colorMap, const Vec3b bgr, int& threshould, int& colorPixels) {
+	Vec3d color = bgr;
+	uchar* colorMapPtr = colorMap.ptr<uchar>(0);
+	bool isInColorMap = false;
+	for (int i = 0; i < colorPixels; i++) {
+		int colorGap = sqrt(pow((colorMapPtr[0] - bgr[0]), 2) + pow((colorMapPtr[1] - bgr[1]), 2) + pow((colorMapPtr[2] - bgr[2]), 2));
+		if (colorGap < threshould) {
+			isInColorMap = true;
+			break;
+		}
+		colorMapPtr += 3;
 	}
-	return resultImage;
+	if (!isInColorMap) {
+		colorMapPtr[0] = bgr[0];
+		colorMapPtr[1] = bgr[1];
+		colorMapPtr[2] = bgr[2];
+		colorPixels++;
+	}
+	return Vec3b(colorMapPtr[0], colorMapPtr[1], colorMapPtr[2]);
 }
+
+Mat getIndexColorImage(const Mat& image, Mat& colorMap) {
+	colorMap = Mat(16, 16, CV_8UC3);
+	Mat indexColorImage = Mat(image.rows, image.cols, CV_8UC3);
+	int threshold = 30;
+	int colorMapPixels = 1;
+	colorMap.at<Vec3b>(0, 0) = image.at<Vec3b>(0, 0);
+	bool IsMaximumColorMap = false;
+	while (!IsMaximumColorMap) {
+		for (int row = 0; row < image.rows; row++) {
+			for (int col = 0; col < image.cols; col++) {
+				const Vec3b originPixel = image.at<Vec3b>(row, col);
+				indexColorImage.at<Vec3b>(row, col) = getColorFromMap(colorMap, originPixel, threshold, colorMapPixels);
+			}
+		}
+	}
+	return indexColorImage;
+}
+
+/*
+Mat getMyColorMap();
+Mat getIndexColorImage(const Mat& image);
+
+Mat getMyColorMap() {
+	Mat myColorMap = Mat(16, 16, CV_8UC3);
+	uchar* p = myColorMap.ptr<uchar>(0);
+	for (int i = 0; i < 256; i += 51) {
+		for (int j = 0; j < 256; j += 51) {
+			for (int k = 0; k < 256; k += 51) {
+				*p++ = i, * p++ = j, * p++ = k;
+			}
+		}
+	}
+	imshow("./Image/myColorMap.png", myColorMap);
+	imwrite("./Image/myColorMap.png", myColorMap);
+	return myColorMap;
+}
+
+Mat getIndexColorImage(const Mat& image) {
+	Mat indexColorImage = Mat(image.rows, image.cols, CV_8UC3);
+	int nCols = image.channels() * image.cols;
+	for (int row = 0; row < image.rows; row++) {
+		const uchar* imagePtr = image.ptr<uchar>(row);
+		uchar* indexColorPtr = indexColorImage.ptr<uchar>(row);
+		for (int col = 0; col < nCols; col++) {
+			uchar originValue = *imagePtr++;
+			int remain = originValue % 51;
+			*indexColorPtr = originValue - remain;
+			if (remain >= 25)
+				(*indexColorPtr) += 51;
+			indexColorPtr++;
+		}
+	}
+	return indexColorImage;
+}*/
