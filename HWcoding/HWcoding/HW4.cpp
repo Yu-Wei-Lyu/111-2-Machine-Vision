872f4f9d2ec384ae814306ee2456c1c235e69081
Yu-Wei, Lyu <awaia732@gmail.com>
2023-05-03 00:28:04 +0800

    HW4 gaussian

--- HWcoding/HWcoding/HW4.cpp
+++ HWcoding/HWcoding/HW4.cpp
@@ -2,16 +2,18 @@
 #include<time.h> 
 #include<cmath>
 
+#define PI 3.14159
+
 using namespace std;
 using namespace cv;
 
 class FilterImage {
 private:
-	const string BINARY_FOLDER = "../Image/Binary/";
+	const string IMAGE_FOLDER = "../Image/";
 	Mat _image, _filerImage;
 	string _name, _ext;
 	int _imageRows, _imageCols;
-	int _layer;
+	vector<double> _gaussianKernel;
 
 public:
 	// 類別初始化
@@ -30,13 +32,13 @@ public:
 		_filerImage = Mat(_imageRows, _imageCols, CV_8UC1);
 	}
 
-	// 用side*side卷積回傳鄰近的像素點
-	vector<uchar> getConvolutionList(int row, int col, int side) {
-		vector<uchar> pixelList(side * side);
+	// 用kernelSize*kernelSize卷積回傳鄰近的像素點
+	vector<uchar> getConvolutionList(int row, int col, int kernelSize) {
+		vector<uchar> pixelList;
 		int pixelListIndex = 0;
-		int start = -side / 2, end = start + side - 1; // if side = 3*3, start = -1, end = 1 etc...
+		int start = -kernelSize / 2, end = start + kernelSize - 1; // if kernelSize = 3*3, start = -1, end = 1 etc...
 		for (int windowRow = start; windowRow <= end; ++windowRow) {
-			for (int windowCol = start; windowCol < start + side; ++windowCol) {
+			for (int windowCol = start; windowCol <= end; ++windowCol) {
 				int nRow = row + windowRow, nCol = col + windowCol;
 				if (nRow >= 0 && nRow < _imageRows && nCol >= 0 && nCol < _imageCols) {
 					pixelList.at(pixelListIndex++) = _image.at<uchar>(nRow, nCol);
@@ -45,14 +47,13 @@ public:
 				}
 			}
 		}
-		int index = 0;
-		for (int i = 0; i < side; i++) {
-			for (int j = 0; j < side; j++) {
-				cout << (int)pixelList.at(index++) << "\t";
-			}
-			cout << endl;
-		}
-		getMedianValue(pixelList);
+		//int index = 0;
+		//for (int i = 0; i < kernelSize; i++) {
+		//	for (int j = 0; j < kernelSize; j++) {
+		//		cout << (int)pixelList.at(index++) << "\t";
+		//	}
+		//	cout << endl;
+		//}
 		return pixelList;
 	}
 
@@ -96,19 +97,62 @@ public:
 		return pixelList.at(medianIndex);
 	}
 
+	// 依照給定的 kernel 大小和標準化參數給出 kernel權重值
+	void setGaussianKernel(int kernelSize, double standardDeviation) {
+		_gaussianKernel.clear();
+		double weightTotal = 0;
+		double kernelIndex = 0;
+		int start = -kernelSize / 2, end = start + kernelSize - 1; // if kernelSize = 3*3, start = -1, end = 1 etc...
+		for (int windowRow = start; windowRow <= end; ++windowRow) {
+			for (int windowCol = start; windowCol <= end; ++windowCol) {
+				double rowSquare = pow(windowRow, 2), colSquare = pow(windowCol, 2);
+				double exponent = -(rowSquare + colSquare) / (2 * pow(standardDeviation, 2));
+				double weight = exp(exponent);
+				//cout << weight  << "\t";
+				_gaussianKernel.at(kernelIndex++) = weight;
+				weightTotal += weight;
+			}
+			//cout << endl;
+		}
+		//cout << "total = " << kernelIndex << endl;
+		//double newWeightTotal = 0;
+		for (double& weight : _gaussianKernel) {
+			weight /= weightTotal;
+			//newWeightTotal += weight;
+		}
+		//kernelIndex = 0;
+		//for (int windowRow = start; windowRow <= end; ++windowRow) {
+		//	for (int windowCol = start; windowCol <= end; ++windowCol) {
+		//		cout << _gaussianKernel.at(kernelIndex++) << "\t";
+		//	}
+		//	cout << endl;
+		//}
+		//cout << "new total = " << newWeightTotal << endl;
+	}
+
+	// 取得卷積範圍內的 Gaussian 值
+	uchar getGaussianValue(vector<uchar> pixelList) {
+		double value = 0;
+		for (int pixelIndex = 0; pixelIndex < pixelList.size(); ++pixelIndex) {
+			value += (double)pixelList.at(pixelIndex) * (double)_gaussianKernel.at(pixelIndex);
+		}
+		return (uchar)round(value);
+	}
+
+	// 取得
 };
 
 int main() {
 	cout << "[Main] Start to processing images, please wait..." << endl;
 
 	// 設定各圖像處理參數
-	vector<string> imageFileList{ "House256_noise.png", "Lena_gray.png", "Mandrill_gray.png", "Peppers_noise.png"};
-
+	//vector<string> imageFileList{ "House256_noise.png", "Lena_gray.png", "Mandrill_gray.png", "Peppers_noise.png"};
+	vector<string> imageFileList{ "House256_noise.png" };
 	// 執行各圖像處理
 	for (const string& imageFile : imageFileList) {
 		Mat image = imread("../Image/Source/" + imageFile);
 		FilterImage filerImage = FilterImage(image, imageFile);
-		filerImage.getConvolutionList(1,1, 7);
+		filerImage.setGaussianKernel(7, 0.707);
 		break;
 	}
 	cout << "[Main] All image processing complete." << endl;
