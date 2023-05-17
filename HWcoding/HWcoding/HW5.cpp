b90fa875646bc542d503af8283b89810849d00bc
Yu-Wei,Lyu <awaia732@gmail.com>
2023-05-17 14:44:53 +0800

    save

--- HWcoding/HWcoding/HW5.cpp
+++ HWcoding/HWcoding/HW5.cpp
@@ -5,6 +5,16 @@
 using namespace std;
 using namespace cv;
 
+typedef enum
+{
+	PREWITT_VERTICAL,
+	PREWITT_HORIZONTAL,
+	SOBEL_VERTICAL,
+	SOBEL_HORIZONTAL,
+	LAPLACIAN_ONE,
+	LAPLACIAN_TWO
+} edge_operator_t;
+
 class EdgeDetector {
 private:
 	const int KERNEL_SIZE = 3;
@@ -15,15 +25,16 @@ private:
 	vector<int> _sobelHorizontal{ -1, -2, -1, 0, 0, 0, 1, 2, 1 };
 	vector<int> _laplacianKernelOne{ 0, 1, 0, 1, -4, 1, 0, 1, 0 };
 	vector<int> _laplacianKernelTwo{ 1, 1, 1, 1, -8, 1, 1, 1, 1 };
+	int rowIndex, colIndex;
 	
 	// 用 3*3 卷積回傳鄰近的像素點
-	vector<uchar> getConvolutionList(const Mat& source, int row, int col) {
+	vector<uchar> getConvolutionList(const Mat& source) {
 		vector<uchar> pixelList(KERNEL_SIZE * KERNEL_SIZE);
 		int pixelListIndex = 0;
 		int start = -KERNEL_SIZE / 2, end = start + KERNEL_SIZE - 1; // kernelSize = 3*3
 		for (int windowRow = start; windowRow <= end; ++windowRow) {
 			for (int windowCol = start; windowCol <= end; ++windowCol) {
-				int nRow = row + windowRow, nCol = col + windowCol;
+				int nRow = rowIndex + windowRow, nCol = colIndex + windowCol;
 				if (nRow >= 0 && nRow < source.rows && nCol >= 0 && nCol < source.cols) {
 					pixelList.at(pixelListIndex++) = source.at<uchar>(nRow, nCol);
 				}
@@ -35,80 +46,68 @@ private:
 		return pixelList;
 	}
 
+	int getEdgeValue(const Mat& source, vector<uchar> pixelList) {
+		int edgeValue = 0;
+		int pixelListIndex = 0;
+		int start = -KERNEL_SIZE / 2, end = start + KERNEL_SIZE - 1; // kernelSize = 3*3
+		for (int windowRow = start; windowRow <= end; ++windowRow) {
+			for (int windowCol = start; windowCol <= end; ++windowCol) {
+				int nRow = rowIndex + windowRow, nCol = colIndex + windowCol;
+				if (nRow >= 0 && nRow < source.rows && nCol >= 0 && nCol < source.cols) {
+					edgeValue += (*_kernel).at(pixelListIndex) * pixelList.at(pixelListIndex);
+				}
+				++pixelListIndex;
+			}
+		}
+		return edgeValue;
+	}
+
 public:
-	string FileName, FileExt;
 
 	EdgeDetector() {	}
 
 	// 類別初始化
-	void transformFile(string file) {
+	void transformFileName(string file, string& fileName, string& fileExt) {
 		size_t dot_pos = file.rfind('.');
-		FileName = file.substr(0, dot_pos);
-		FileExt = file.substr(dot_pos);
-	}
-
-	// 依照給定的 kernel 大小和標準化參數給出 kernel權重值
-	void setGaussianKernel(int kernelSize, double standardDeviation) {
-		_gaussianKernel = vector<double>(kernelSize * kernelSize);
-		double weightTotal = 0;
-		double kernelIndex = 0;
-		int start = -kernelSize / 2, end = start + kernelSize - 1; // if kernelSize = 3*3, start = -1, end = 1 etc...
-		for (int windowRow = start; windowRow <= end; ++windowRow) {
-			for (int windowCol = start; windowCol <= end; ++windowCol) {
-				double rowSquare = pow(windowRow, 2), colSquare = pow(windowCol, 2);
-				double exponent = -(rowSquare + colSquare) / (2 * pow(standardDeviation, 2));
-				double weight = exp(exponent);
-				_gaussianKernel.at(kernelIndex++) = weight;
-				weightTotal += weight;
-			}
-		}
-		double newWeightTotal = 0;
-		for (double& weight : _gaussianKernel) {
-			weight /= weightTotal;
-		}
+		fileName = file.substr(0, dot_pos);
+		fileExt = file.substr(dot_pos);
 	}
 
 	// 設定灰階化圖像
-	void updateGrayScaleImage(Mat& source) {
-		Mat grayImage = Mat(source.rows, source.cols, CV_8UC1);
+	void updateGrayScaleImage(const Mat& source, Mat& dest) {
+		dest.create(source.rows, source.cols, CV_8UC1);
 		const uchar* imagePtr;
 		uchar* gray;
 		for (int row = 0; row < source.rows; ++row) {
 			imagePtr = source.ptr<uchar>(row);
-			gray = grayImage.ptr<uchar>(row);
+			gray = dest.ptr<uchar>(row);
 			for (int col = 0; col < source.cols; ++col) {
 				uchar blue = *imagePtr++, green = *imagePtr++, red = *imagePtr++;
 				*gray++ = (0.3 * red) + (0.59 * green) + (0.11 * blue);
 			}
 		}
-		grayImage.copyTo(source);
 	}
 
 	// 取得特定方法過濾圖像
-	void getFilterImageByMode(Mat& source, Mat& dest, const string& mode, int kernelSize) {
-		if (mode == "mean") _getValueFunc = &FilterImage::getMeanValue;
-		else if (mode == "median") _getValueFunc = &FilterImage::getMedianValue;
-		else if (mode == "gaussian") _getValueFunc = &FilterImage::getGaussianValue;
-		dest = Mat(source.rows, source.cols, CV_8UC1);
+	void getEdgeImageByMethod(Mat& source, Mat& dest, edge_operator_t edgeOperator, int threshold) {
+		if (edgeOperator == PREWITT_VERTICAL) _kernel = &_prewittVertical;
+		else if (edgeOperator == PREWITT_HORIZONTAL) _kernel = &_prewittHorizontal;
+		else if (edgeOperator == SOBEL_VERTICAL) _kernel = &_sobelVertical;
+		else if (edgeOperator == SOBEL_HORIZONTAL) _kernel = &_sobelHorizontal;
+		else if (edgeOperator == LAPLACIAN_ONE) _kernel = &_laplacianKernelOne;
+		else if (edgeOperator == LAPLACIAN_TWO) _kernel = &_laplacianKernelTwo;
+		dest.create(source.rows, source.cols, CV_8UC1);
+		dest = Mat::zeros(source.rows, source.cols, CV_8UC1);
 		uchar* destPtr;
-		for (int row = 0; row < source.rows; ++row) {
-			destPtr = dest.ptr<uchar>(row);
-			for (int col = 0; col < source.cols; ++col) {
-				vector<uchar> pixelList = getConvolutionList(source, row, col, kernelSize);
-				*destPtr++ = (this->*_getValueFunc)(pixelList);
+		for (rowIndex = 0; rowIndex < source.rows; ++rowIndex) {
+			destPtr = dest.ptr<uchar>(rowIndex);
+			for (colIndex = 0; colIndex < source.cols; ++colIndex) {
+				vector<uchar> pixelList = getConvolutionList(source);
+				int edgeValue = getEdgeValue(source, pixelList);
+				if (abs(edgeValue) >= threshold) *destPtr = 255;
+				++destPtr;
 			}
 		}
-	}
-};
-
-class FilterKernel {
-public:
-	string Mode;
-	vector<int> KernelSize;
-
-	FilterKernel(string mode, vector<int> size) {
-		this->Mode = mode;
-		this->KernelSize = size;
 	}
 };
 
@@ -117,44 +116,30 @@ int main() {
 
 	// 設定各圖像處理參數
 	vector<string> imageFileList{ 
-		"House256_noise.png", 
-		"Lena_gray.png", 
-		"Mandrill_gray.png", 
-		"Peppers_noise.png"
-	};
-	vector<FilterKernel> filterKernel{
-		FilterKernel("mean", { 3, 7 }),
-		FilterKernel("median", { 3, 7 }),
-		FilterKernel("gaussian", { 5 })
+		"House512.png", 
+		//"Lena.png", 
+		//"Mandrill.png"
 	};
 	
+	vector<int> thresholdList{
+		200
+	};
+
 	// 執行各圖像處理
-	double processing = 0, previousPercent = 0, currentPercent = 0, processTotal = 140;
-	FilterImage filter;
-	filter.setGaussianKernel(5, 1.414);
+	EdgeDetector edgeDetector;
 	for (const string& sourceImageFile : imageFileList) {
 		const Mat sourceImage = imread("../Image/Source/" + sourceImageFile);
-		filter.transformFile(sourceImageFile);
-		for (const FilterKernel& fk : filterKernel) {
-			for (const int& kernelSize : fk.KernelSize) {
-				Mat resultImage = Mat(sourceImage.rows, sourceImage.cols, CV_8UC1);
-				Mat sampleImage;
-				sourceImage.copyTo(sampleImage);
-				filter.updateGrayScaleImage(sampleImage);
-				for (int repeat = 1; repeat <= 7; ++repeat) {
-					filter.getFilterImageByMode(sampleImage, resultImage, fk.Mode, kernelSize);
-					string saveFilePath = "../Image/" + filter.FileName + "/";
-					string saveFileName = filter.FileName + "(" + fk.Mode + to_string(kernelSize) + "x" + to_string(kernelSize) + "r" + to_string(repeat) + ")" + filter.FileExt;
-					imwrite(saveFilePath + saveFileName, resultImage);
-					resultImage.copyTo(sampleImage);
-					currentPercent = round((++processing / processTotal) * 100);
-					if (previousPercent != currentPercent) {
-						cout << "\r" << "Processing : " << currentPercent << "%";
-					}
-					previousPercent = processing;
-				}
-			}
-		}
+		string fileName, fileExt;
+		edgeDetector.transformFileName(sourceImageFile, fileName, fileExt);
+		cout << fileName << " and " << fileExt << endl;
+		Mat grayImage;
+		edgeDetector.updateGrayScaleImage(sourceImage, grayImage);
+		imshow(fileName, grayImage);
+		Mat resultImage;
+		edgeDetector.getEdgeImageByMethod(grayImage, resultImage, PREWITT_VERTICAL, 200);
+		imshow(fileName + "prewitt vertical", resultImage);
+		edgeDetector.getEdgeImageByMethod(grayImage, resultImage, PREWITT_HORIZONTAL, 200);
+		imshow(fileName + "prewitt horizontal", resultImage);
 	}
 	cout << "\n[Main] All image processing complete." << endl;
 	cv::waitKey();
