10f44782a15f52134e2e7ac0dea03e28677acb7d
Yu-Wei,Lyu <awaia732@gmail.com>
2023-05-18 15:31:46 +0800

    image output

--- HWcoding/HWcoding/HW5.cpp
+++ HWcoding/HWcoding/HW5.cpp
@@ -5,12 +5,6 @@
 using namespace std;
 using namespace cv;
 
-enum class FilterMethod {
-	MEAN,
-	MEDIAN,
-	GAUSSIAN
-};
-
 enum class EdgeOperatorName {
 	PREWITT_VERTICAL,
 	PREWITT_HORIZONTAL,
@@ -22,19 +16,20 @@ enum class EdgeOperatorName {
 	LAPLACIAN_TWO
 };
 
-class FilterImage {
+class GassianFilter {
 private:
+	const int KERNEL_SIZE = 3;
 	vector<double> _gaussianKernel;
-	uchar(FilterImage::* _getValueFunc)(vector<uchar>) = nullptr;
+	int rowIndex, colIndex;
 
 	// 用kernelSize*kernelSize卷積回傳鄰近的像素點
-	vector<uchar> getConvolutionList(const Mat& source, int row, int col, int kernelSize) {
-		vector<uchar> pixelList(kernelSize * kernelSize);
+	vector<uchar> getConvolutionList(const Mat& source) {
+		vector<uchar> pixelList(KERNEL_SIZE * KERNEL_SIZE);
 		int pixelListIndex = 0;
-		int start = -kernelSize / 2, end = start + kernelSize - 1; // if kernelSize = 3*3, start = -1, end = 1 etc...
+		int start = -KERNEL_SIZE / 2, end = start + KERNEL_SIZE - 1; // if kernelSize = 3*3, start = -1, end = 1 etc...
 		for (int windowRow = start; windowRow <= end; ++windowRow) {
 			for (int windowCol = start; windowCol <= end; ++windowCol) {
-				int nRow = row + windowRow, nCol = col + windowCol;
+				int nRow = rowIndex + windowRow, nCol = colIndex + windowCol;
 				if (nRow >= 0 && nRow < source.rows && nCol >= 0 && nCol < source.cols) {
 					pixelList.at(pixelListIndex++) = source.at<uchar>(nRow, nCol);
 				}
@@ -44,41 +39,6 @@ private:
 			}
 		}
 		return pixelList;
-	}
-
-	// 取得卷積範圍內的 Mean 值
-	uchar getMeanValue(vector<uchar> pixelList) {
-		int value = 0;
-		int pixelAmount = pixelList.size();
-		for (int pixelIndex = 0; pixelIndex < pixelAmount; ++pixelIndex) {
-			value += (int)pixelList.at(pixelIndex);
-		}
-		return round((double)value / pixelAmount);
-	}
-
-	// 快速排序函数
-	void quickSort(vector<uchar>& nums, int left, int right) {
-		if (left >= right) return;
-		int i = left, j = right, pivot = nums[left + (right - left) / 2];
-		while (i <= j) {
-			while (nums[i] < pivot) i++;
-			while (nums[j] > pivot) j--;
-			if (i <= j) {
-				swap(nums[i], nums[j]);
-				i++;
-				j--;
-			}
-		}
-		quickSort(nums, left, j);
-		quickSort(nums, i, right);
-	}
-
-	// 取得卷積範圍內排序後的 Median 值
-	uchar getMedianValue(vector<uchar> pixelList) {
-		quickSort(pixelList, 0, pixelList.size() - 1);
-		int index = 0;
-		int medianIndex = (pixelList.size() - 1) / 2;
-		return pixelList.at(medianIndex);
 	}
 
 	// 取得卷積範圍內的 Gaussian 值
@@ -91,16 +51,17 @@ private:
 	}
 
 public:
-	string FileName, FileExt;
-
-	FilterImage() {	}
+	GassianFilter() {
+		rowIndex = 0;
+		colIndex = 0;
+	}
 
 	// 依照給定的 kernel 大小和標準化參數給出 kernel權重值
-	void setGaussianKernel(int kernelSize, double standardDeviation) {
-		_gaussianKernel = vector<double>(kernelSize * kernelSize);
+	void setGaussianKernel(double standardDeviation) {
+		_gaussianKernel = vector<double>(KERNEL_SIZE * KERNEL_SIZE);
 		double weightTotal = 0;
 		double kernelIndex = 0;
-		int start = -kernelSize / 2, end = start + kernelSize - 1; // if kernelSize = 3*3, start = -1, end = 1 etc...
+		int start = -KERNEL_SIZE / 2, end = start + KERNEL_SIZE - 1; // if kernelSize = 3*3, start = -1, end = 1 etc...
 		for (int windowRow = start; windowRow <= end; ++windowRow) {
 			for (int windowCol = start; windowCol <= end; ++windowCol) {
 				double rowSquare = pow(windowRow, 2), colSquare = pow(windowCol, 2);
@@ -116,37 +77,18 @@ public:
 		}
 	}
 
-	// 設定灰階化圖像
-	void updateGrayScaleImage(Mat& source) {
-		Mat grayImage = Mat(source.rows, source.cols, CV_8UC1);
-		const uchar* imagePtr;
-		uchar* gray;
-		for (int row = 0; row < source.rows; ++row) {
-			imagePtr = source.ptr<uchar>(row);
-			gray = grayImage.ptr<uchar>(row);
-			for (int col = 0; col < source.cols; ++col) {
-				uchar blue = *imagePtr++, green = *imagePtr++, red = *imagePtr++;
-				*gray++ = (0.3 * red) + (0.59 * green) + (0.11 * blue);
-			}
-		}
-		grayImage.copyTo(source);
-	}
-
 	// 取得特定方法過濾圖像
-	void getFilterImageByMode(Mat& source, Mat& dest, FilterMethod mode, int kernelSize, int deep) {
+	void getGaussianFilterImageByDeep(Mat& source, Mat& dest, int deep) {
 		Mat referenceImage, processImage;
-		if (mode == FilterMethod::MEAN) _getValueFunc = &FilterImage::getMeanValue;
-		else if (mode == FilterMethod::MEDIAN) _getValueFunc = &FilterImage::getMedianValue;
-		else if (mode == FilterMethod::GAUSSIAN) _getValueFunc = &FilterImage::getGaussianValue;
 		source.copyTo(referenceImage);
 		processImage.create(source.rows, source.cols, CV_8UC1);
 		for (int repeat = 0; repeat < deep; ++repeat) {
 			uchar* processPtr;
-			for (int row = 0; row < source.rows; ++row) {
-				processPtr = processImage.ptr<uchar>(row);
-				for (int col = 0; col < source.cols; ++col) {
-					vector<uchar> pixelList = getConvolutionList(referenceImage, row, col, kernelSize);
-					*processPtr++ = (this->*_getValueFunc)(pixelList);
+			for (rowIndex = 0; rowIndex < source.rows; ++rowIndex) {
+				processPtr = processImage.ptr<uchar>(rowIndex);
+				for (colIndex = 0; colIndex < source.cols; ++colIndex) {
+					vector<uchar> pixelList = getConvolutionList(referenceImage);
+					*processPtr++ = getGaussianValue(pixelList);
 				}
 			}
 			processImage.copyTo(referenceImage);
@@ -205,7 +147,7 @@ private:
 	}
 
 	bool isOverThreshold(int threshold, int value1, int value2) {
-		int tangent = sqrt(pow(value1, 2) + pow(value2, 2));
+		int tangent = abs(value1) + abs(value2);
 		return tangent >= threshold;
 	}
 
@@ -283,7 +225,6 @@ public:
 		size_t dot_pos = name.rfind('.');
 		this->FileName = name.substr(0, dot_pos);
 		this->FileExt = name.substr(dot_pos);
-
 	}
 
 	// 類別初始化
@@ -298,42 +239,42 @@ int main() {
 
 	// 設定各圖像處理參數
 	vector<ImageInfo> imageList{
-		//ImageInfo("House512.png", 70, 70, 10, 25 ),
-		ImageInfo("Lena.png", 80, 95, 8, 20),
-		//ImageInfo("Mandrill.png", 70, 70, 10, 25)
+		//ImageInfo("House512.png", 60, 80, 10, 25 ),
+		//ImageInfo("Lena.png", 80, 95, 8, 20),
+		ImageInfo("Mandrill.png", 90, 110, 8, 25)
 	};
 
 	// 執行各圖像處理
 	EdgeDetector edgeDetector;
-	FilterImage filter;
-	filter.setGaussianKernel(3, 0.707);
+	GassianFilter filter;
+	filter.setGaussianKernel(1.414);
 	for (ImageInfo& image : imageList) {
 		const Mat sourceImage = imread("../Image/Source/" + image.FileName + image.FileExt);
 		Mat grayImage;
 		edgeDetector.updateGrayScaleImage(sourceImage, grayImage);
 		imshow(image.FileName, grayImage);
 		Mat resultImage;
-		edgeDetector.getEdgeImageByMethod(grayImage, resultImage, EdgeOperatorName::PREWITT_VERTICAL, image.PrewittThreshold);
+		Mat gaussianImage, laplacianImage;
+		filter.getGaussianFilterImageByDeep(grayImage, gaussianImage, 2);
+		edgeDetector.getEdgeImageByMethod(gaussianImage, resultImage, EdgeOperatorName::PREWITT_VERTICAL, image.PrewittThreshold);
 		//imshow(image.FileName + " prewitt vertical", resultImage);
 		image.write("prewitt_vertical", resultImage);
-		edgeDetector.getEdgeImageByMethod(grayImage, resultImage, EdgeOperatorName::PREWITT_HORIZONTAL, image.PrewittThreshold);
+		edgeDetector.getEdgeImageByMethod(gaussianImage, resultImage, EdgeOperatorName::PREWITT_HORIZONTAL, image.PrewittThreshold);
 		//imshow(image.FileName + " prewitt horizontal", resultImage);
 		image.write("prewitt_horizontal", resultImage);
-		edgeDetector.getEdgeImageByMethod(grayImage, resultImage, EdgeOperatorName::PREWITT_BOTH, image.PrewittThreshold);
+		edgeDetector.getEdgeImageByMethod(gaussianImage, resultImage, EdgeOperatorName::PREWITT_BOTH, image.PrewittThreshold);
 		imshow(image.FileName + " prewitt both", resultImage);
 		image.write("prewitt_both", resultImage);
-		edgeDetector.getEdgeImageByMethod(grayImage, resultImage, EdgeOperatorName::SOBEL_VERTICAL, image.SobelThreshold);
+		edgeDetector.getEdgeImageByMethod(gaussianImage, resultImage, EdgeOperatorName::SOBEL_VERTICAL, image.SobelThreshold);
 		//imshow(image.FileName + " sobel vertical", resultImage);
 		image.write("sobel_vertical", resultImage);
-		edgeDetector.getEdgeImageByMethod(grayImage, resultImage, EdgeOperatorName::SOBEL_HORIZONTAL, image.SobelThreshold);
+		edgeDetector.getEdgeImageByMethod(gaussianImage, resultImage, EdgeOperatorName::SOBEL_HORIZONTAL, image.SobelThreshold);
 		//imshow(image.FileName + " sobel horizontal", resultImage);
 		image.write("sobel_horizontal", resultImage);
-		edgeDetector.getEdgeImageByMethod(grayImage, resultImage, EdgeOperatorName::SOBEL_BOTH, image.SobelThreshold);
+		edgeDetector.getEdgeImageByMethod(gaussianImage, resultImage, EdgeOperatorName::SOBEL_BOTH, image.SobelThreshold);
 		imshow(image.FileName + " sobel both", resultImage);
 		image.write("sobel_both", resultImage);
-		
-		Mat gaussianImage, laplacianImage;
-		filter.getFilterImageByMode(grayImage, gaussianImage, FilterMethod::GAUSSIAN, 3, 2);
+
 		//imshow(image.FileName + " gaussian", gaussianImage);
 		edgeDetector.getEdgeImageByMethod(gaussianImage, resultImage, EdgeOperatorName::LAPLACIAN_ONE, image.LaplacianOneThreshold);
 		imshow(image.FileName + " laplacian 1", resultImage);
